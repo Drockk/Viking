9b8becea6b55a4116a056817ad59d93d60c72d34
Bartosz Zielonka <batziell@gmail.com>
2022-08-23 17:08:41 +0200

    Added Event class

---
+++ Viking/Source/Viking/Events/Event.hpp
@@ -0,0 +1,70 @@
+#pragma once
+#include <functional>
+
+#include "Viking/Core/Base.hpp"
+
+namespace Viking {
+    //Events in Viking are currently blocking, meaning when an event occurs it immediately gets dispatched and must be dealt with right then an there.
+    //For the future, a better strategy might be to buffer events in an event bus and process them during the "event" part of the update stage.
+
+    enum class EventType {
+        None = 0,
+        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased, KeyTyped,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+    };
+
+    enum EventCategory {
+        None = 0,
+        EventCategoryApplication	= BIT(0),
+        EventCategoryInput			= BIT(1),
+        EventCategoryKeyboard       = BIT(2),
+        EventCategoryMouse          = BIT(3),
+        EventCategoryMouseButton    = BIT(4)
+    };
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; } \
+    virtual EventType GetEventType() const override { return GetStaticType(); } \
+    virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+    class Event {
+    public:
+        virtual ~Event() = default;
+
+        bool Handled{ false };
+
+        virtual EventType getEventType() const = 0;
+        virtual const char* getName() const = 0;
+        virtual int getCategoryFlags() const = 0;
+        virtual std::string toString() const { return getName(); }
+
+        bool isInCategory(EventCategory category) const {
+            return getCategoryFlags() & category;
+        }
+    };
+
+    class EventDispatcher {
+    public:
+        EventDispatcher(Event& event): m_Event(event) {}
+
+        //F will be deduced by the compiler
+        template<typename T, typename F>
+        bool dispatch(const F& func) {
+            if (m_Event.getEventType() == T::getStaticType()) {
+                m_Event.Handled |= func(static_cast<T&>(m_Event));
+                return true;
+            }
+
+            return false;
+        }
+    private:
+        Event& m_Event;
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const Event& e) {
+        return os << e.toString();
+    }
+}
