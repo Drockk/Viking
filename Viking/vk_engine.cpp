8be071585315224583470e1dd4b2b1f391acf864
Bartosz Zielonka <batziell@gmail.com>
2023-09-10 11:36:50 +0200

    vulkan init

--- Viking/vk_engine.cpp
+++ Viking/vk_engine.cpp
@@ -3,7 +3,20 @@
 #include "vk_types.hpp"
 
 #include <GLFW/glfw3.h>
+
 #include <stdexcept>
+#include <iostream>
+
+#define VK_CHECK(x)                                                 \
+    do                                                              \
+    {                                                               \
+        VkResult err = x;                                           \
+        if (err)                                                    \
+        {                                                           \
+            std::cout <<"Detected Vulkan error: " << err << '\n';   \
+            abort();                                                \
+        }                                                           \
+    } while (0)
 
 void ViEngine::init()
 {
@@ -20,12 +33,28 @@ void ViEngine::init()
 
     glfwMakeContextCurrent(m_window);
 
+    //load the core Vulkan structures
+    initVulkan();
+    initSwapchain();
+
     m_isInitialized = true;
 }
 
 void ViEngine::cleanup()
 {
     if (m_isInitialized) {
+        vkDestroySwapchainKHR(m_device, m_swapchain, nullptr);
+
+        //destroy swapchain resources
+        for (int i = 0; i < m_swapchainImageViews.size(); i++) {
+
+            vkDestroyImageView(m_device, m_swapchainImageViews[i], nullptr);
+        }
+
+        vkDestroyDevice(m_device, nullptr);
+        vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+        vkb::destroy_debug_utils_messenger(m_instance, m_debug_messenger);
+        vkDestroyInstance(m_instance, nullptr);
         glfwDestroyWindow(m_window);
         glfwTerminate();
     }
@@ -42,4 +71,65 @@ void ViEngine::run()
         glfwPollEvents();
         draw();
     }
+}
+
+void ViEngine::initSwapchain()
+{
+    vkb::SwapchainBuilder swapchainBuilder{ m_device2, m_surface };
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        .use_default_format_selection()
+        //use vsync present mode
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(m_windowExtent.width, m_windowExtent.height)
+        .build()
+        .value();
+
+    //store swapchain and its related images
+    m_swapchain = vkbSwapchain.swapchain;
+    m_swapchainImages = vkbSwapchain.get_images().value();
+    m_swapchainImageViews = vkbSwapchain.get_image_views().value();
+
+    m_swapchainImageFormat = vkbSwapchain.image_format;
+}
+
+void ViEngine::initVulkan()
+{
+    vkb::InstanceBuilder builder;
+    //make the Vulkan instance, with basic debug features
+    auto inst_ret = builder.set_app_name("ViEngine")
+        .request_validation_layers(true)
+        .require_api_version(1, 1, 0)
+        .use_default_debug_messenger()
+        .build();
+
+    auto& vkb_inst = inst_ret.value();
+
+    //store the debug messenger
+    m_instance = vkb_inst.instance;
+
+    //store the debug messenger
+    m_debug_messenger = vkb_inst.debug_messenger;
+
+    // get the surface of the window we opened with glfw
+    if (glfwCreateWindowSurface(m_instance, m_window, nullptr, &m_surface)) {
+        throw std::runtime_error("Cannot create GLFW window surface");
+    }
+
+    //use vkbootstrap to select a GPU.
+    //We want a GPU that can write to the SDL surface and supports Vulkan 1.1
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    vkb::PhysicalDevice physicalDevice = selector
+        .set_minimum_version(1, 1)
+        .set_surface(m_surface)
+        .select()
+        .value();
+
+    //create the final Vulkan device
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+
+    m_device2 = deviceBuilder.build().value();
+
+    // Get the VkDevice handle used in the rest of a Vulkan application
+    m_device = m_device2.device;
+    m_chosenGPU = physicalDevice.physical_device;
 }
--- Viking/vk_engine.hpp
+++ Viking/vk_engine.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "vk_types.hpp"
 
+#include <VkBootstrap.h>
+
 struct GLFWwindow;
 
 class ViEngine
@@ -12,8 +14,30 @@ public:
     VkExtent2D m_windowExtent{ 1600, 900 };
     GLFWwindow* m_window{ nullptr };
 
+    VkSwapchainKHR m_swapchain; // from other articles
+
+    // image format expected by the windowing system
+    VkFormat m_swapchainImageFormat;
+
+    //array of images from the swapchain
+    std::vector<VkImage> m_swapchainImages;
+
+    //array of image-views from the swapchain
+    std::vector<VkImageView> m_swapchainImageViews;
+
+    VkInstance m_instance; // Vulkan library handle
+    VkDebugUtilsMessengerEXT m_debug_messenger; // Vulkan debug output handle
+    VkPhysicalDevice m_chosenGPU; // GPU chosen as the default device
+    VkDevice m_device; // Vulkan device for commands
+    vkb::Device m_device2;
+    VkSurfaceKHR m_surface; // Vulkan window surface
+
     void init();
     void cleanup();
     void draw();
     void run();
+
+private:
+    void initSwapchain();
+    void initVulkan();
 };
